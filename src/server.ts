b492cf40e7e042a449cd3cf109f321a1160a4f5c
Pablo Otoboni <pablootoboni@gmail.com>
2026-06-28 22:25:47 -0300

    feat: created inital controller

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import * as http from "http"
+import { getListEpisodes } from './controllers/podcast-controller'
 
 const server = http.createServer(
-    (request: http.IncomingMessage, response: http.ServerResponse) => {
-
+    async (req: http.IncomingMessage, res: http.ServerResponse) => {
+        if (req.method === "GET") {
+            await getListEpisodes(req, res);
+        }
     })
 
 const port = process.env.PORT
