a075d590a3a3a37ad87ac77c1e4bd7f708a19d30
Pablo Otoboni <pablootoboni@gmail.com>
2026-06-28 22:43:17 -0300

    feat: Created inicial service function

--- src/controllers/podcast-controller.ts
+++ src/controllers/podcast-controller.ts
@@ -1,9 +1,11 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { serviceListEpisodes } from "../services/list_episodes-service"
 
 export const getListEpisodes =
     async (req: IncomingMessage, res: ServerResponse) => {
-        res.writeHead(200, {'Content-Type': "application/json"})
-        res.end(JSON.stringify({
-            name: "Pablo"
-        }))
+
+        const content = await serviceListEpisodes()
+
+        res.writeHead(200, { 'Content-Type': "application/json" })
+        res.end(JSON.stringify(content))
     }
---
+++ src/services/list_episodes-service.ts
@@ -0,0 +1,7 @@
+export const serviceListEpisodes = async () => {
+    const data = {
+        name: "Pablo Otoboni"
+    }
+
+    return data
+}
